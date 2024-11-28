0445831aa639e7392e3cd0335a68410529c928be
datnt04 <ndat020304@gmail.com>
2024-11-28 18:15:12 +0700

    update

--- ss1/Buoi_1/src/bai_1/CurrencyConverter.java
+++ ss1/Buoi_1/src/bai_1/CurrencyConverter.java
@@ -4,10 +4,19 @@
  */
 package bai_1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ACER
  */
 public class CurrencyConverter {
-    
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số tiền USD: ");
+        String input = scanner.nextLine();  
+        double usd = Double.parseDouble(input);
+        double vnd = usd * 23000;  
+        System.out.printf("%.2f USD = %.2f VND%n", usd, vnd);
+    }
 }
