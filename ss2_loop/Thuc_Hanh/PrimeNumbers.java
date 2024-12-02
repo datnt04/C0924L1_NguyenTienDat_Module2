18f445de175c1c515e8622c6d81b0f866c4b1b21
datnt04 <ndat020304@gmail.com>
2024-12-02 08:59:46 +0700

    update

---
+++ ss2_loop/Thuc_Hanh/PrimeNumbers.java
@@ -0,0 +1,25 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int count = 0;
+        int number = 2;
+
+        System.out.println("20 số nguyên tố đầu tiên là:");
+        while (count < 20) {
+            boolean isPrime = true;
+
+            for (int i = 2; i <= number / 2; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(number + " ");
+                count++;
+            }
+
+            number++;
+        }
+    }
+}
---
+++ ss2_loop/Thuc_Hanh/SimpleShapes.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class SimpleShapes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập chiều dài (số dòng): ");
+        int height = scanner.nextInt();
+
+        System.out.print("Nhập chiều rộng (số cột): ");
+        int width = scanner.nextInt();
+
+        System.out.println("\nHình chữ nhật:");
+        for (int i = 0; i < height; i++, System.out.println())
+            for (int j = 0; j < width; j++)
+                System.out.print("* ");
+        System.out.println("\nHình tam giác vuông (cạnh góc vuông ở bottom-left):");
+        for (int i = 1; i <= height; i++, System.out.println())
+            for (int j = 1; j <= i; j++)
+                System.out.print("* ");
+
+        System.out.println("\nHình tam giác vuông (cạnh góc vuông ở top-left):");
+        for (int i = height; i >= 1; i--, System.out.println())
+            for (int j = 1; j <= i; j++)
+                System.out.print("* ");
+
+        System.out.println("\nHình tam giác cân:");
+        for (int i = 1; i <= height; i++, System.out.println()) {
+            for (int j = 1; j <= height - i; j++)
+                System.out.print("  ");
+            for (int k = 1; k <= 2 * i - 1; k++)
+                System.out.print("* ");
+        }
+
+        scanner.close();
+    }
+}
